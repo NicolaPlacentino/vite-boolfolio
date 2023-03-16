30ca926cba40c6f83cb3ea482f438e55376d94fa
NicolaPlacentino <nyko0921@gmail.com>
2023-03-16 16:40:07 +0100

    created router and imported it in main.js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
+import { router } from './router'
 
 library.add(faEye)
 
 const app = createApp(App);
 
 app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(router)
 
 app.mount('#app');
